e5478d994cb6adaab12337756bbcf07ac90762ba
Slurpd1 <104402459+Slurpd1@users.noreply.github.com>
2024-08-15 22:51:41 -0600

    Created function getComputerChoice() which will randomly generate and return a string representing either rock paper or scissors.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // generate a random number between 0 and 2
+    // use the randomly generated number to choose and return
+    // either rock paper or scissors.
+    let randomNum;
+    let computerChoice;
+    
+    randomNum = Math.floor(Math.random()*3);
+
+    // now decision is either 0 1 or 2.
+    // lets say 0 is rock, 1 is paper, 2 is scissors.
+
+    if (randomNum == 0) {
+        computerChoice = "rock"; 
+    }
+
+    else if (randomNum == 1) {
+        computerChoice = "paper"; 
+    }
+
+    else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
